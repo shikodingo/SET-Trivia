907936eab51133beeffe8ecafc614fc237162e7f
Nathan-Nickel <nthnnckl@yahoo.ca>
2015-11-27 10:02:19 -0500

    begun sudo coding most major requirements are sudo coded

--- WMP+RD/TriviaService/TriviaService.cs
+++ WMP+RD/TriviaService/TriviaService.cs
@@ -18,6 +18,21 @@ namespace TriviaService
     public partial class TriviaService : ServiceBase
     {
 
+        /*
+         * create a default main folder in the C:\
+         * create service, user, admin files within the main file
+         * files meant for users will be sent to user file, files meant for admin will be sent to admin file, and files coming back to service will go in service file
+         * when a user is sending a file back the users name will be in the file header to identify them
+         * when the game starts a file for each question will be sent to the user folder. these files will also include all answers for that question
+         * the question files will have the question number in the file title to identify them
+         * the users will need to parse the data in the file to take out the question and seperate the answers
+         * once the user has answered a question they will send a file to the service folder with the question # in the title and the answer in the file
+         * the location of the main folder and there for the 3 sub folders will be able to  be changed by the admin with a file in the service folder called directory
+         * the service will keep track of user scores by reading in the answers put into the serivce folder and storeing them in the UsersAnswers table
+         * when a user is created they will amek a new file in service folder called "newUser" with their user name in the file. the service will store these users in the Users MySQL table
+         * 
+         */
+
         delegate void MyCallback(Object obj);       // Delegate declaration for use in Invoke
         FileSystemWatcher fsw;
         const string directory = @"message\";//puts message into message folder under same directory as the exe   
@@ -53,7 +68,7 @@ namespace TriviaService
             string fileData = ReadFile(e.FullPath, out status);
             if (status == "OK")
             {
-                Thread t1 = new Thread(new ParameterizedThreadStart(SetLblInvoke));
+                Thread t1 = new Thread(new ParameterizedThreadStart(SetLblInvoke));//test sending to file
                 t1.Start(fileData);//currently overwriting message need to make it use += not =
                 if (fileData == "Shutdown")
                 {
@@ -109,7 +124,7 @@ namespace TriviaService
             for (int i=1; i<=10; i++)//currently this creates a file for each question with the file having a randomly generated title and the question as text in the file
             {
                 string question = accessData.SelectAQuestion(i);
-                createNewMessage.WriteData(question, );//write question into file
+                createNewMessage.WriteData(question, "temp");//write question into file
             }
             
         }
