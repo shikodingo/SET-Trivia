cb32e13180b219b5c016f788f1f944e00eb08dbf
Nathan-Nickel <nthnnckl@yahoo.ca>
2015-11-29 00:42:51 -0500

    new for WMP+RD side

---
+++ WMP+RD/WMP+RD/Logging.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WMP_RD
+{
+    public static class Logging
+    {
+
+        //string className, string methodName, 
+        public static void Log(string eventDetails)
+        {
+            EventLog serviceEventLog = new EventLog();
+            string formattedS = "SETTrivia" + "-" + eventDetails;
+
+            if (!EventLog.SourceExists("SETTrivia"))
+            {
+                EventLog.CreateEventSource("SETTrivia", "SETTriviaEvents");
+            }
+            serviceEventLog.Source = "SETTrivia";
+            serviceEventLog.Log = "SETTriviaEvents";
+
+            serviceEventLog.WriteEntry(formattedS);
+        }
+    }
+}
