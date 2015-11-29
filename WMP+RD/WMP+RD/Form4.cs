2bf6e51cbddae20d9f68bdb8545d6a061c6030da
Nathan-Nickel <nthnnckl@yahoo.ca>
2015-11-29 13:23:03 -0500

    started working on changing question

--- WMP+RD/WMP+RD/Form4.cs
+++ WMP+RD/WMP+RD/Form4.cs
@@ -12,6 +12,9 @@ namespace WMP_RD
 {
     public partial class frmEdit : Form
     {
+        string directory = @"C:\Users\Nathan\OneDrive\Windows and Mobile Programming\Assign 6\main\";
+        const string watchExt = @"Admin\";
+        const string writeExt = @"Service\";
         public frmEdit()
         {
             InitializeComponent();
@@ -20,6 +23,42 @@ namespace WMP_RD
         private void btnSave_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void cmbQuestionNum_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string qNum = cmbQuestionNum.SelectedItem.ToString();
+            string question = txtQuestionDesc.Text;
+            string answerA = txtA.Text;
+            string answerB = txtB.Text;
+            string answerC = txtC.Text;
+            string answerD = txtD.Text;
+            string extension = writeExt;
+            string questionPlusAnswers = "";
+            if (question == "")
+            {
+                question = "*";
+            }
+            if (answerA == "")
+            {
+                answerA = "*";
+            }
+            if (answerB == "")
+            {
+                answerB = "*";
+            }
+            if (answerC == "")
+            {
+                answerC = "*";
+            }
+            if (answerD == "")
+            {
+                answerD = "*";
+            }
+            IPCFileProducer createNewMessage = new IPCFileProducer();
+                questionPlusAnswers = qNum + "|" + question + "|";
+                questionPlusAnswers += answerA + "|" + answerB + "|" + answerC + "|" + answerD;
+                createNewMessage.WriteData(questionPlusAnswers, "changeQuestionAnswer", directory + extension);//write question into file
         }
     }
 }
